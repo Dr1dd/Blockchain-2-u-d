6c4ed8a93add1a06234f02e342ebae68d2619d74
Laurynas Leišis <lauris.leisis@gmail.com>
2019-10-15 15:46:10 +0300

    added Blockchain classes

--- blockchain.h
+++ blockchain.h
@@ -2,19 +2,45 @@
 #define BLOCKCHAIN_H
 
 #include<vector>
+#include <ctime>
 using std::string;
 
 class User {
     public:
         string name;
         string public_key;
-        int balance;
+        double balance;
 };
 class Transactions{
     public:
+        string _sender;
+        string _receiver;
+        double _value;
+        std::time_t _date;
+
+        Transactions();
+        Transactions(string sender, string receiver, double value){
+            _sender = sender;
+            _receiver= receiver;
+            _value = value;
+            _date = std::time(nullptr);
+
+        }
 
 
 };
+class Block{
+    string _previousBlock;
+    std::time_t Timestamp;
+    string version = "0.1";
+    string merkelHash;
+    unsigned int Nonce;
+    double difficultyTarget;
+
+    std::vector<Transactions> TransactionBlock;
+
+};
+
 string Hashish(string &);
 User GenerateUser(int i);
 void MainFunction();
