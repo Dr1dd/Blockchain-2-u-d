a0665c67c062ebf9c3ee29efbf5ecb9fca480c48
Laurynas Leišis <lauris.leisis@gmail.com>
2019-11-21 21:47:54 +0200

    v0.3

--- Blockchain.cpp
+++ Blockchain.cpp
@@ -137,52 +137,7 @@ void MainFunction(){
     }
 
 }
-/*bc::hash_digest create_merkle(bc::hash_list& merkle)
-{
-	// Stop if hash list is empty or contains one element
-	if (merkle.empty())
-		return bc::null_hash;
-	else if (merkle.size() == 1)
-		return merkle[0];
 
-	// While there is more than 1 hash in the list, keep looping...
-	while (merkle.size() > 1)
-	{
-		// If number of hashes is odd, duplicate last hash in the list.
-		if (merkle.size() % 2 != 0)
-			merkle.push_back(merkle.back());
-		// List size is now even.
-		assert(merkle.size() % 2 == 0);
-
-		// New hash list.
-		bc::hash_list new_merkle;
-		// Loop through hashes 2 at a time.
-		for (auto it = merkle.begin(); it != merkle.end(); it += 2)
-		{
-			// Join both current hashes together (concatenate).
-			bc::data_chunk concat_data(bc::hash_size * 2);
-			auto concat = bc::serializer<
-				decltype(concat_data.begin())>(concat_data.begin());
-			concat.write_hash(*it);
-			concat.write_hash(*(it + 1));
-			// Hash both of the hashes.
-			bc::hash_digest new_root = bc::bitcoin_hash(concat_data);
-			// Add this to the new list.
-			new_merkle.push_back(new_root);
-		}
-		// This is the new list.
-		merkle = new_merkle;
-
-		// DEBUG output -------------------------------------
-		std::cout << "Current merkle hash list:" << std::endl;
-		for (const auto& hash : merkle)
-			std::cout << "  " << bc::encode_base16(hash) << std::endl;
-		std::cout << std::endl;
-		// --------------------------------------------------
-	}
-	// Finally we end up with a single item.
-	return merkle[0];
-}*/
 std::vector<string> MerkleTree(std::vector<string> myTransaction){
     string result;
     std::vector<string> TransactionTemp;
@@ -222,18 +177,26 @@ Block newBlock(std::vector<Block> myBlockchain, std::vector<Transactions> myTran
     myBlock.setTransactionBlock(tempTransactionBlock);
     std::vector<string> MerkleHash;
 
+    if(!myTransactionsHashes.empty()){
+        if(myTransactionsHashes.size()==1) MerkleHash = myTransactionsHashes; // v0.3
+        else MerkleHash = MerkleTree(myTransactionsHashes);
+    }
 
-    MerkleHash = MerkleTree(myTransactionsHashes);
-
-    myBlock.setMerkleHash(MerkleHash[0]);
+    if(!MerkleHash.empty())
+        myBlock.setMerkleHash(MerkleHash[0]);
 
     myBlock.setTimestamp(std::time(nullptr));
 
     string MainBlockHash;
     string diffTarget = "00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
     myBlock.setDifficultyTarget(diffTarget);
-
-    return myBlock;
+    if(!MerkleHash.empty()){
+        return myBlock;
+    }
+    else{
+        std:: cout << "Nerasta transakciju " << std::endl;
+        std::exit(0);
+    }
 }
 Block mineBlock(Block myBlock, int maxNonce, int Nonce){
     string TempBlockHash;
